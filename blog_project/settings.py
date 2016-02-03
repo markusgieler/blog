92e2389b95e88e679367ba2c2ecafbe2bd86d73d
blog <gieler.markus@gmail.com>
2016-02-03 14:52:05 -0200

    Added views to create/edit blog post inside the site.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), 
+                 os.path.join(BASE_DIR, 'blog/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
